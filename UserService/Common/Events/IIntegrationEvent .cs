f77501b9cc9c80480a8557d9563d615ec1210c09
BogdanJ33 <bogdanjanosevic@gmail.com>
2025-11-30 19:39:35 +0100

    fix(events): Removed unnecessary event

--- UserService/Common/Events/IIntegrationEvent .cs
+++ UserService/Common/Events/IIntegrationEvent .cs
@@ -2,5 +2,4 @@
 {
     public interface IIntegrationEvent { }
     public record UserDeletedIntegrationEvent(Guid UserId, string Role) : IIntegrationEvent;
-    public record ReservationCreatedIntegrationEvent(Guid UserId, string Role, string Check) : IIntegrationEvent;
 }
