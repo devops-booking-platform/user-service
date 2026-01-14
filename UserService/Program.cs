71d82bdc0e2fa414960fbff636e3a236973af056
BogdanJ33 <bogdanjanosevic@gmail.com>
2026-01-14 21:02:14 +0100

    feat(tracing): Implemented rabbitmq tracing

--- UserService/Program.cs
+++ UserService/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
+using OpenTelemetry.Context.Propagation;
 using Serilog;
 using System.Text;
 using UserService.Configuration;
@@ -13,6 +14,27 @@ builder.Host.UseSerilog((ctx, lc) => lc
     .ReadFrom.Configuration(ctx.Configuration)
     .Enrich.FromLogContext()
 );
+var compositeTextMapPropagator = new CompositeTextMapPropagator(new TextMapPropagator[]
+{
+    new TraceContextPropagator(),
+    new BaggagePropagator()
+});
+Sdk.SetDefaultTextMapPropagator(compositeTextMapPropagator);
+
+builder.Services.AddOpenTelemetry()
+    .WithTracing(tracing =>
+    {
+        tracing
+            .SetResourceBuilder(
+                ResourceBuilder.CreateDefault().AddService("UserService"))
+            .AddAspNetCoreInstrumentation()
+            .AddHttpClientInstrumentation()
+            .AddRabbitMQInstrumentation()
+            .AddOtlpExporter(o =>
+            {
+                o.Endpoint = new Uri("http://jaeger:4317");
+            });
+    });
 builder.Services.Configure<JwtSettings>(builder.Configuration.GetSection("Jwt"));
 var jwtSettings = builder.Configuration.GetSection("Jwt").Get<JwtSettings>();
 
